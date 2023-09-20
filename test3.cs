967dd040d3a6a8693a6c738d30e0ef1b19c78a5b
Emanuel Ribeiro <55888010+imma-cx@users.noreply.github.com>
2023-09-20 15:01:25 +0100

    Update test3.cs

--- test3.cs
+++ test3.cs
@@ -2,10 +2,13 @@ using System;
 
 namespace SimpleCalculator
 {
+    
     public class InputConverter
     {
+        
         public double ConvertInputToNumeric(string input)
         {
+            
             double convertedNumber;
             bool isConversionSuccessfull = double.TryParse(input, out convertedNumber);
 
